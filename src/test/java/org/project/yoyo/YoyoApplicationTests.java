eb5ca433df88a161127a053595f4f6b64fe26276
minkyung kim <minry20415@naver.com>
2025-01-22 13:37:58 +0900

    ✨: init project

---
+++ src/test/java/org/project/yoyo/YoyoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.yoyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
